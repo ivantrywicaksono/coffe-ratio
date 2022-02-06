f1d08a11dca4e3accc99f792ed29339b916d5137
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2022-02-06 18:32:44 +0700

    memperbaiki script

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var ratio = document.getElementById("coffeeRatio").value
-var material = document.getElementById("material").value
-var amount = document.getElementById("amount")
+var r = document.getElementById("coffeeRatio")
+var ratio = r.options[r.selectedIndex].value
+var m = document.getElementById("material")
+var material = m.options[m.selectedIndex].value
+var amount = document.getElementById("amount").value
 var resultCoffee = amount * ratio
 var resultWater = amount / ratio
-function coffeeRatio(material) {
+function coffeeRatio() {
     if (material == "coffee") {
         document.getElementById("result").innerHTML = resultCoffee;
     } else {
